78881e51a1fccbbc21620561eb0d06a9b2dff799
Gijela <linjianhui@bixin.cn>
2023-12-26 19:17:57 +0800

    feat: 【插件注释】提供 react 项目编译和热更新的功能

--- 00-构建工具/vite/vite-project/vite.config.ts
+++ 00-构建工具/vite/vite-project/vite.config.ts
@@ -3,5 +3,5 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react()], // plugins 数组中默认配置了官方的 react 插件，来提供 react 项目编译和热更新的功能
 })
