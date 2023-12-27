a64155c3953d693bd61ab305922016ed918f3df9
Gijela <linjianhui@bixin.cn>
2023-12-27 22:59:23 +0800

    feat: 预构建

--- 00-构建工具/vite/vite-project/vite.config.ts
+++ 00-构建工具/vite/vite-project/vite.config.ts
@@ -9,5 +9,19 @@ export default defineConfig({
     alias: {
       '@assets': path.join(__dirname, 'src/assets')
     }
+  },
+  // 预构建自定义配置
+  optimizeDeps: {
+    // 入口文件。值为一个字符串数组，支持各种格式入口，比如 html, js, jsx, ts, tsx
+    entries: ['js', 'jsx', 'ts', 'tsx', 'html'],
+    // note: vite 会根据入口文件(entries)自动搜集依赖，然后进行预构建，然而 vite 默认的扫描行为并不完全可靠，
+    // 这就需要联合配置 include 来达到完美的预构建配置效果。
+    // 强制执行预构建的依赖项。值为一个数组字符串。
+    // include: ['lodash-es', 'vue'], // 将 lodash-es 和 vue 两个包强制进行预构建
+    include: [],
+    // 自定义 Esbuild 行为
+    esbuildOptions: {
+      plugins: [] // 加入的一些插件，通常用来处理第三方包代码问题
+    }
   }
 })
