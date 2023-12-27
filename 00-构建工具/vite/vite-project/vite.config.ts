6a402b358820a45d4adcc2589ccf15a13cee630d
Gijela <linjianhui@bixin.cn>
2023-12-27 18:24:26 +0800

    feat: lint & 路径别名

--- 00-构建工具/vite/vite-project/vite.config.ts
+++ 00-构建工具/vite/vite-project/vite.config.ts
@@ -1,7 +1,13 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import path from 'path'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()], // plugins 数组中默认配置了官方的 react 插件，来提供 react 项目编译和热更新的功能
+  resolve: {
+    alias: {
+      '@assets': path.join(__dirname, 'src/assets')
+    }
+  }
 })
